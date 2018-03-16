65b779a7bc82233453f13ba5cb750b52cba25cd6
Jagan Langa S - சாமி. ஜெகன் லங்கா <seesamjagan@yahoo.co.in>
2018-03-16 18:29:13 +0530

    initial commit
    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div id="message">
+          <h2>Welcome</h2>
+          <h1>SeeSamJagan - Firebase Hosting Setup Complete</h1>
+          <p>You're seeing this because you've successfully setup Firebase Hosting. Now it's time to go build something extraordinary!</p>
+          <a target="_blank" href="https://firebase.google.com/docs/hosting/">Open Hosting Documentation</a>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
